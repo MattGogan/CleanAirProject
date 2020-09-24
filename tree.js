a540bbfbcd5f415c8072f695449be05e246daf3d
MattGogan <55172084+MattGogan@users.noreply.github.com>
2020-09-24 14:26:08 -0400

    Adding Trees Programatically

--- tree.js
+++ tree.js
@@ -1,6 +1,8 @@
 let prompts = [];
 let options = [];
+let trees = [];
 let questionNumber = 0;
+let netCarbonEmissions = 0.0; //lbs
 
 
 window.onload = function(){
@@ -9,7 +11,7 @@ window.onload = function(){
     var fadeInHeading = "<br><h1 class = \"w3-animate-opacity\">";
 
     prompts = [
-        fadeInHeading+"<b>This is a question about carbon consumption</b></h1>",
+        fadeInHeading+"<b>This is a question about carbon consumption</b></h1><br>This first tree cleans the CO2 that you exhale in a year.  One mature tree can support two humans",
         fadeInHeading+"This is another question",
         fadeInHeading+"Do you have a car?",
         fadeInHeading+"Do you know your car's average gas mileage?",
@@ -17,9 +19,9 @@ window.onload = function(){
     ];
 
     options = [
-        "<button class = \"w3-animate-opacity\" onclick=\"nextQuestion()\">LessTrees</button>&nbsp;<button class = \"w3-animate-opacity\" onclick=\"nextQuestion()\">MoreTrees</button>",
-        "<form class=\"w3-container w3-card-4 w3-animate-opacity\"><p><input class=\"w3-check\" type=\"checkbox\" checked=\"checked\"><label> I like trees</label></p><p><input class=\"w3-check\" type=\"checkbox\"><label> I'm unopinionated on trees</label></p><p><input class=\"w3-check\" type=\"checkbox\" disabled><label>I hate trees (Disabled)</label></p></form><br><button onclick = \"nextQuestion()\">Submit</button>",
-        "<button class = \"w3-animate-opacity\" onclick=\"nextQuestion()\">Yes</button>&nbsp;<button class = \"w3-animate-opacity\" onclick=\"nextQuestion()\">No</button>",
+        buttonClassString() + "onclick=\"nextQuestion(10)\">LessTrees</button>&nbsp;" +buttonClassString() + "onclick=\"nextQuestion(1000)\">MoreTrees</button>",
+        "<form class=\"w3-container w3-card-4 w3-animate-opacity\"><p><input class=\"w3-check\" type=\"checkbox\" checked=\"checked\"><label> I like trees</label></p><p><input class=\"w3-check\" type=\"checkbox\"><label> I'm unopinionated on trees</label></p><p><input class=\"w3-check\" type=\"checkbox\" disabled><label>I hate trees (Disabled)</label></p></form><br><button onclick = \"nextQuestion(200)\">Submit</button>",
+        "<button class = \"w3-animate-opacity\" onclick=\"nextQuestion(5000)\">Yes</button>&nbsp;<button class = \"w3-animate-opacity\" onclick=\"nextQuestion()\">No</button>",
         "<button class = \"w3-animate-opacity\" onclick=\"nextQuestion()\">Yes</button>&nbsp;<button class = \"w3-animate-opacity\" onclick=\"nextQuestionShowEPA()\">No</button>",
         "<button class = \"w3-animate-opacity\" onclick=\"nextQuestion()\">Move On</button>"
     ];
@@ -27,15 +29,26 @@ window.onload = function(){
     go();
 }
 
+function buttonClassString(){
+    return '<button class = "w3-animate-opacity, w3-button w3-green"';
+}
+
 
 
 function go(){
     
-    nextQuestion();
+    addFirstTree();
+    nextQuestion(0.0);
     
 }
 
-function nextQuestion(){
+function nextQuestion(carbonEmissionAdd){
+    netCarbonEmissions += carbonEmissionAdd;
+    console.log("Running carbon emissions total for quiz at: " + netCarbonEmissions + "lbs CO2");
+
+    //Where the divisor represents how many lbs CO2 = one tree
+    addTrees(carbonEmissionAdd/48);
+
     var divOptions = document.getElementById("divOptions");
     var divPrompts = document.getElementById("divPrompts");
     
@@ -44,6 +57,57 @@ function nextQuestion(){
     
     questionNumber++;
 }
+
+//Where x is the number of trees to add to the frame
+function addTrees(x){
+    var renderTrees = document.getElementById("divRenderTrees");
+
+    for(i = 0; i<x*2; i++){
+    
+    var t = document.createElement("img");
+    t.setAttribute("src", "tree.png");
+    t.setAttribute = ('class', 'absolute');
+    t.classList.add("absolute");
+    t.classList.add("fade-in");
+    
+
+    var right = window.innerWidth * Math.random()-50;
+    var top = window.innerHeight * Math.random()-100;
+
+
+    t.style.right = right+"px";
+    t.style.top = top+"px";
+
+
+    trees.push(t);
+    renderTrees.appendChild(t);
+    document.body.appendChild(t);
+
+    }
+}
+
+function addFirstTree(){
+    var renderTrees = document.getElementById("divRenderTrees");
+    
+    var t = document.createElement("img");
+    t.setAttribute("src", "tree.png");
+    t.setAttribute = ('class', 'absolute');
+    t.classList.add("absolute");
+    t.classList.add("fade-in");
+    
+
+    var right = window.innerWidth/2;
+    var top = window.innerHeight/2;
+
+
+    t.style.right = right+"px";
+    t.style.top = top+"px";
+
+    trees.push(t);
+    renderTrees.appendChild(t);
+    document.body.appendChild(t);
+}
+
 
 function nextQuestionShowEPA(){
     var divOptions = document.getElementById("divOptions");
